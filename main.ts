9b340a8ee3a04d8f29ab982c3e18bff687e1f0fc
Trevor Shaw <shawt@genlrn.com>
2018-11-25 16:16:07 -0500

    Updates.

--- main.ts
+++ main.ts
@@ -6,4 +6,29 @@ basic.showLeds(`
     . # # # .
     `);
 
+/**
+ * Functions are mapped to blocks using various macros 
+ * in comments starting with % (e.g., //% block).
+ * The most important macro "block" specifies that a
+ * block should be generated for a **exported** function.
+ */
+//% color="#AA278D"
+namespace bitbooster {
+    //% block
+    export function servoControl() {
 
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function dcMotor() {
+
+    }
+
+    //% block
+    export function grove() {
+
+    }
+}
