53eb8c0016cac37e74f85a7b5dbee73e328f8dc7
Trevor Shaw <shawt@genlrn.com>
2018-11-25 21:05:08 -0500

    Updates.

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ namespace booster {
 
     /**
       * Get the word field editor
-      * @param word eg: H
+      * @param word eg: "H"
       */
     //% blockId=wordPicker block="$word"
     //% blockHidden=true
